9bef81019f2d49eeefabbff47aa74b35173400e9
Alyssa Holland <alysam0071@gmail.com>
2024-06-30 15:37:55 -0400

    Scaffold landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,119 @@
-import Image from "next/image";
-
-export default function Home() {
-	console.log("Home");
+export default function Example() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-					Get started by editing&nbsp;
-					<code className="font-mono font-bold">
-						src/app/page.tsx
-					</code>
-				</p>
-				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-					<a
-						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"></a>
+		<div className="bg-white">
+			<header className="absolute inset-x-0 top-0 z-50">
+				<nav
+					className="flex items-center justify-between p-6 lg:px-8"
+					aria-label="Global">
+					<div className="flex lg:flex-1">
+						<a href="#" className="-m-1.5 p-1.5">
+							<span className="sr-only">Your Company</span>
+							<img
+								className="h-8 w-auto"
+								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								alt=""
+							/>
+						</a>
+					</div>
+
+					<div className=" lg:flex lg:flex-1 lg:justify-end">
+						<a
+							href="#"
+							className="text-sm font-semibold leading-6 text-gray-900">
+							Log in <span aria-hidden="true">&rarr;</span>
+						</a>
+					</div>
+				</nav>
+			</header>
+
+			<div className="relative isolate pt-14">
+				<svg
+					className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+					aria-hidden="true">
+					<defs>
+						<pattern
+							id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
+							width={200}
+							height={200}
+							x="50%"
+							y={-1}
+							patternUnits="userSpaceOnUse">
+							<path d="M100 200V.5M.5 .5H200" fill="none" />
+						</pattern>
+					</defs>
+					<svg
+						x="50%"
+						y={-1}
+						className="overflow-visible fill-gray-50">
+						<path
+							d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+							strokeWidth={0}
+						/>
+					</svg>
+					<rect
+						width="100%"
+						height="100%"
+						strokeWidth={0}
+						fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
+					/>
+				</svg>
+				<div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+					<div className="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
+						<h1 className="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+							A better way to find music
+						</h1>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							Esse id magna consectetur fugiat non dolor in ad
+							laboris magna laborum ea consequat. Nisi irure
+							aliquip nisi adipisicing veniam voluptate id. In
+							veniam incididunt ex veniam adipisicing sit.
+						</p>
+						<div className="mt-10 flex items-center gap-x-6">
+							<a
+								href="#"
+								className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+								Get started
+							</a>
+							<a
+								href="#"
+								className="text-sm font-semibold leading-6 text-gray-900">
+								Learn more <span aria-hidden="true">→</span>
+							</a>
+						</div>
+					</div>
+					<div className="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+						<svg
+							viewBox="0 0 366 729"
+							role="img"
+							className="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
+							<title>App screenshot</title>
+							<defs>
+								<clipPath id="2ade4387-9c63-4fc4-b754-10e687a0d332">
+									<rect width={316} height={684} rx={36} />
+								</clipPath>
+							</defs>
+							<path
+								fill="#4B5563"
+								d="M363.315 64.213C363.315 22.99 341.312 1 300.092 1H66.751C25.53 1 3.528 22.99 3.528 64.213v44.68l-.857.143A2 2 0 0 0 1 111.009v24.611a2 2 0 0 0 1.671 1.973l.95.158a2.26 2.26 0 0 1-.093.236v26.173c.212.1.398.296.541.643l-1.398.233A2 2 0 0 0 1 167.009v47.611a2 2 0 0 0 1.671 1.973l1.368.228c-.139.319-.314.533-.511.653v16.637c.221.104.414.313.56.689l-1.417.236A2 2 0 0 0 1 237.009v47.611a2 2 0 0 0 1.671 1.973l1.347.225c-.135.294-.302.493-.49.607v377.681c0 41.213 22 63.208 63.223 63.208h95.074c.947-.504 2.717-.843 4.745-.843l.141.001h.194l.086-.001 33.704.005c1.849.043 3.442.37 4.323.838h95.074c41.222 0 63.223-21.999 63.223-63.212v-394.63c-.259-.275-.48-.796-.63-1.47l-.011-.133 1.655-.276A2 2 0 0 0 366 266.62v-77.611a2 2 0 0 0-1.671-1.973l-1.712-.285c.148-.839.396-1.491.698-1.811V64.213Z"
+							/>
+							<path
+								fill="#343E4E"
+								d="M16 59c0-23.748 19.252-43 43-43h246c23.748 0 43 19.252 43 43v615c0 23.196-18.804 42-42 42H58c-23.196 0-42-18.804-42-42V59Z"
+							/>
+							<foreignObject
+								width={316}
+								height={684}
+								transform="translate(24 24)"
+								clipPath="url(#2ade4387-9c63-4fc4-b754-10e687a0d332)">
+								<img
+									src="https://tailwindui.com/img/component-images/mobile-app-screenshot.png"
+									alt=""
+								/>
+							</foreignObject>
+						</svg>
+					</div>
 				</div>
-				<img />
-				<label>this is the label</label>
-				<input type="checkbox" name="check" />
 			</div>
-
-			<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-				<a
-					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className="mb-3 text-2xl font-semibold">
-						Docs{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className="m-0 max-w-[30ch] text-sm opacity-50">
-						Find in-depth information about Next.js features and
-						API.
-					</p>
-				</a>
-
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className="mb-3 text-2xl font-semibold">
-						Learn{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className="m-0 max-w-[30ch] text-sm opacity-50">
-						Learn about Next.js in an interactive course
-						with&nbsp;quizzes!
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className="mb-3 text-2xl font-semibold">
-						Templates{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className="m-0 max-w-[30ch] text-sm opacity-50">
-						Explore starter templates for Next.js.
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					target="_blank"
-					rel="noopener noreferrer">
-					<h2 className="mb-3 text-2xl font-semibold">
-						Deploy{" "}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-						Instantly deploy your Next.js site to a shareable URL
-						with Vercel.
-					</p>
-				</a>
-			</div>
-		</main>
+		</div>
 	);
 }
