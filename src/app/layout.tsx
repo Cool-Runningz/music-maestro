95d6cf34632218845a3494b2b2f232ae8ea85a04
Alyssa Holland <alysam0071@gmail.com>
2024-07-26 01:36:12 -0400

    Add metadata tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,34 @@ import MusicProvider from "@/contexts/MusicContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title =
+	"Music Maestro - Craft custom playlists based on any search criteria.";
+const description =
+	"Leverage AI to curate a playlist of songs and discover music on Spotify.";
+const imageUrl =
+	"https://raw.githubusercontent.com/Cool-Runningz/music-maestro/main/public/music-maestro-landing-page-90s.png";
+
 export const metadata: Metadata = {
-	title: "Music Maestro - Craft custom playlists based on any search criteria.",
-	description:
-		"Leverage AI to curate a playlist of songs and discover music on Spotify.",
+	title,
+	description,
+	openGraph: {
+		type: "website",
+		url: "https://musicmaestro.io",
+		title,
+		description,
+		images: [
+			{
+				url: imageUrl,
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		title,
+		description,
+		creator: "@alyssa_codes",
+		images: [imageUrl],
+	},
 };
 
 export default function RootLayout({
